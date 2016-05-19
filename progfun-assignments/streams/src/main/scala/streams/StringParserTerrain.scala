c21d8fb0ebd04159b2d3bd6a44b1e3e32e867933
ThibaultClement <thibault.clem@gmail.com>
2016-05-19 15:50:12 +0200

    Finish stream assignement

--- progfun-assignments/streams/src/main/scala/streams/StringParserTerrain.scala
+++ progfun-assignments/streams/src/main/scala/streams/StringParserTerrain.scala
@@ -52,7 +52,13 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = {
+    ((pos: Pos) => (pos.x >= 0)
+      && (pos.y >= 0)
+      && (levelVector.size > pos.x)
+      && (levelVector(pos.x).size > pos.y)
+      && (List('o', 'S', 'T') contains levelVector(pos.x)(pos.y)))
+  }
 
   /**
    * This function should return the position of character `c` in the
@@ -62,7 +68,10 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+    val row = levelVector.indexWhere(_.contains(c))
+    Pos(row, levelVector(row).indexOf(c))
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
--- progfun-assignments/streams/src/test/scala/streams/BloxorzSuite.scala
+++ progfun-assignments/streams/src/test/scala/streams/BloxorzSuite.scala
@@ -62,6 +62,42 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("legal blocks at start") {
+    new Level1 {
+      assert(startBlock.legalNeighbors == List((Block(Pos(2,1),Pos(3,1)),Down), (Block(Pos(1,2),Pos(1,3)),Right)))
+    }
+  }
+
+  test("neighbors with history") {
+    new Level1 {
+      assert(neighborsWithHistory(Block(Pos(1,1),Pos(1,1)), List(Left,Up)).toSet == Set(
+        (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      ))
+    }
+  }
+
+  test("new Neighbors Only") {
+    new Level1 {
+      assert(newNeighborsOnly(
+        Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        ).toStream,
+
+        Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+      ) == Set(
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      ).toStream)
+    }
+  }
+
+  test("from") {
+    new Level1 {
+      assert(from(Stream((startBlock, List())), Set())(1) == (Block(Pos(2,1),Pos(3,1)),List(Down)))
+    }
+  }
+
 
 	test("optimal solution for level 1") {
     new Level1 {
